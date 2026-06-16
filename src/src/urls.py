dbe2fb9072027535123971132e74ae2c3ce4135a
Aliaksei <fanikov41@gmail.com>
2026-06-16 19:59:12 +0300

    add new urls

--- src/src/urls.py
+++ src/src/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from app.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(r'', index, name='index')
+    path(r'indexes/', include('app.urls')),
 ]
