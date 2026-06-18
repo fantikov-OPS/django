943c57ea20458fb2c159b414bfedb24c32cc3d8f
Aliaksei <fanikov41@gmail.com>
2026-06-18 19:24:38 +0300

    add settings

--- src/app/models.py
+++ src/app/models.py
@@ -12,17 +12,3 @@ class Customer(models.Model):
 
     def __str__(self):
         return f'{self.firstname} {self.lastname}'
-
-
-class CustomerV2(models.Model):
-    firstname = models.CharField('Имя', max_length=255)
-    lastname = models.CharField('Фамилия', max_length=255)
-    age = models.PositiveIntegerField('Возраст')
-    profession = models.CharField('Профессия', max_length=255)
-
-    class Meta:
-        verbose_name = 'Клиент'
-        verbose_name_plural = 'Клиенты'
-
-    def __str__(self):
-        return f'{self.firstname} {self.lastname}'
